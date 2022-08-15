5ae77906f76537d829c7ef8bb1c15aa1b5f3eb71
Gayathri-0918 <108792184+Gayathri-0918@users.noreply.github.com>
2022-08-15 20:51:10 +0530

    Add files via upload

---
+++ Whdemo3.java
@@ -0,0 +1,43 @@
+package practice;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Whdemo3 {
+
+	public static void main(String[] args) 
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\maven project selenium\\project1\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.seleniumframework.com/Practiceform/");
+		driver.manage().window().maximize();
+		String parent=driver.getWindowHandle();
+		System.out.println(parent);
+		driver.findElement(By.xpath("//button[text()='New Browser Tab']")).click();
+		Set<String> allwindows=driver.getWindowHandles();
+		System.out.println(allwindows);
+		Iterator<String> itr=allwindows.iterator();
+		while(itr.hasNext())
+		{
+			String child_window=itr.next();
+
+
+			if(!parent.equals(child_window))
+			{
+			driver.switchTo().window(child_window);
+
+			System.out.println(driver.switchTo().window(child_window).getTitle());
+			driver.close();
+		}
+
+	}
+
+
+	}
+
+}
